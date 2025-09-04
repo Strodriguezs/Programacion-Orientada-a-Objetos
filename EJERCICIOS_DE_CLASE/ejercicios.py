d4bc4f80995100df0f765869c8e2447f94cc005a
Stive Rodriguez <strodriguezs@unal.edu.co>
2025-09-03 20:33:21 -0500

    Primer ejercicio, impresion de texto

---
+++ EJERCICIOS_DE_CLASE/ejercicios.py
@@ -0,0 +1,9 @@
+# Autor : Stive Ferney Rodriguez Silva
+# En este documento se encuentran las diferentes actividades propuestas durante la clase con el objetivo de nivelar al grupo con los contenidos de la materia
+
+# 1.1 Imprimir por pantalla
+
+#1) Escribir un programa en Python que imprima por pantalla la frase “Hola, ya se imprimir frases”.
+
+print("Hola, ya sé imprimir frases")
+
